12a5fc273a23e150c7c247b4fc32f4168743eba8
eshakkhan29 <eshakkhan29@gmail.com>
2022-05-31 19:53:58 +0600

    first commit

---
+++ src/components/HomePage/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Banner.css';
+import Header from '../Header/Header';
+
+const Banner = () => {
+    return (
+        <section className='banner-section'>
+            <Header></Header>
+        </section>
+    );
+};
+
+export default Banner;
