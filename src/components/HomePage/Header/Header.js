35be73db313713c18247415d8656881e1634328a
eshakkhan29 <eshakkhan29@gmail.com>
2022-05-31 22:12:12 +0600

    navbar and nabvar responsive

--- src/components/HomePage/Header/Header.js
+++ src/components/HomePage/Header/Header.js
@@ -1,9 +1,45 @@
 import React from 'react';
-
+import './Header.css';
+import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../../image/logo.png';
+import { GoSearch } from 'react-icons/go';
+import { BiChat } from 'react-icons/bi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className='py-0 py-lg-4 container '>
+            <div className='mobile-logo-section'>
+                <div className='mobile-logo'>
+                    <img src={logo} alt="" />
+                </div>
+                <div className='d-flex flex-column flex-lg-row navigation-bar'>
+                    <div className='w-100 bg-black menu-sm'>
+                        <p className='text-white fs-1'><GiHamburgerMenu className='me-3' />Menu</p>
+                    </div>
+                    <div className='nav-items m-auto flex-column flex-lg-row d-flex align-items-lg-center'>
+                        <Link to="/home">Home +</Link>
+                        <Link to="/about">About us</Link>
+                        <Link to="/about">Pages +</Link>
+                        <Navbar.Brand className='logo-lg' to="/">
+                            <img src={logo} alt="" />
+                        </Navbar.Brand>
+                        <Link to="/services">Services</Link>
+                        <Link to="/blog">Blog  +</Link>
+                        <Link to="/contact">Contact</Link>
+                    </div>
+                    <div className='mt-2 pe-2 pe-lg-0 mt-lg-0 pt-3 pt-lg-0 d-flex nav-contact align-items-center'>
+                        <Link className='fs-4 text-white search-icon mx-3' to="/"><GoSearch /></Link>
+                        <div className='d-flex justify-content-between'>
+                            <Link className='fs-3 text-white mx-3' to="/"><BiChat /></Link>
+                            <div className='text-white'>
+                                <p className='m-0 fw-bold'>CALL US</p>
+                                <p className='m-0 fw-bold'>+123-987458741</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
