b859b8f3634a64e177a8c252189a063c287926f2
eshakkhan29 <eshakkhan29@gmail.com>
2022-06-01 11:43:18 +0600

    banner added

--- src/components/HomePage/Banner/Banner.js
+++ src/components/HomePage/Banner/Banner.js
@@ -6,6 +6,23 @@ const Banner = () => {
     return (
         <section className='banner-section'>
             <Header></Header>
+            <div className='container p-0'>
+                <div className='row g-0'>
+                    <div className='col-lg-6'>
+                        <div className='p-lg-0 ps-5'>
+                            <h1 className='banner-title mb-3 text-white '>Be wise & take  our advice</h1>
+                            <p className='heading-p'>Serenity has taken possession of my entire soul</p>
+                        </div>
+                        <div className='ms-lg-0 text-end me-5'>
+                            <button className='btn-main'>Get In Touch</button>
+                        </div>
+                    </div>
+                    <div className='col-lg-6'>
+
+                    </div>
+
+                </div>
+            </div>
         </section>
     );
 };
--- src/components/HomePage/Header/Header.js
+++ src/components/HomePage/Header/Header.js
@@ -8,34 +8,34 @@ import { BiChat } from 'react-icons/bi';
 import { GiHamburgerMenu } from 'react-icons/gi';
 const Header = () => {
     return (
-        <div className='py-0 py-lg-4 container '>
-            <div className='mobile-logo-section'>
-                <div className='mobile-logo'>
-                    <img src={logo} alt="" />
+        <div className='py-0 py-lg-4 container position-relative overflow-hidden'>
+            <div className='mobile-logo'>
+                <img src={logo} alt="" />
+            </div>
+            <p className='text-white menu-icon m-0'><GiHamburgerMenu className='me-3 position-absolute d-lg-none mobile-menu-toggler end-0' /></p>
+            <div className='d-flex flex-column flex-lg-row navigation-bar'>
+                <div className='d-flex align-items-center d-lg-none bg-black menu-sm'>
+                    <p className='text-white menu-icon m-0'><GiHamburgerMenu className='me-3' /></p>
+                    <p className='text-white menu-icon m-0'>Menu</p>
                 </div>
-                <div className='d-flex flex-column flex-lg-row navigation-bar'>
-                    <div className='w-100 bg-black menu-sm'>
-                        <p className='text-white fs-1'><GiHamburgerMenu className='me-3' />Menu</p>
-                    </div>
-                    <div className='nav-items m-auto flex-column flex-lg-row d-flex align-items-lg-center'>
-                        <Link to="/home">Home +</Link>
-                        <Link to="/about">About us</Link>
-                        <Link to="/about">Pages +</Link>
-                        <Navbar.Brand className='logo-lg' to="/">
-                            <img src={logo} alt="" />
-                        </Navbar.Brand>
-                        <Link to="/services">Services</Link>
-                        <Link to="/blog">Blog  +</Link>
-                        <Link to="/contact">Contact</Link>
-                    </div>
-                    <div className='mt-2 pe-2 pe-lg-0 mt-lg-0 pt-3 pt-lg-0 d-flex nav-contact align-items-center'>
-                        <Link className='fs-4 text-white search-icon mx-3' to="/"><GoSearch /></Link>
-                        <div className='d-flex justify-content-between'>
-                            <Link className='fs-3 text-white mx-3' to="/"><BiChat /></Link>
-                            <div className='text-white'>
-                                <p className='m-0 fw-bold'>CALL US</p>
-                                <p className='m-0 fw-bold'>+123-987458741</p>
-                            </div>
+                <div className='nav-items m-auto flex-column flex-lg-row d-flex align-items-lg-center'>
+                    <Link to="/home">Home +</Link>
+                    <Link to="/about">About us</Link>
+                    <Link to="/about">Pages +</Link>
+                    <Navbar.Brand className='logo-lg' to="/">
+                        <img src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Link to="/services">Services</Link>
+                    <Link to="/blog">Blog  +</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
+                <div className='mt-4 mt-lg-0 pt-3 pt-lg-0 d-flex nav-contact align-items-center justify-content-center'>
+                    <Link className='fs-4 text-white search-icon mx-3' to="/"><GoSearch /></Link>
+                    <div className='d-flex justify-content-between'>
+                        <Link className='fs-3 text-white mx-3' to="/"><BiChat /></Link>
+                        <div className='text-white'>
+                            <p className='m-0 fw-bold'>CALL US</p>
+                            <p className='m-0 fw-bold'>+123-987458741</p>
                         </div>
                     </div>
                 </div>
